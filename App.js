1bba5ddf68d05d06961609aa8b7da71aa306eb12
Han Nie <nie.han@northeastern.edu>
2024-09-12 12:39:36 -0700

    in class activity - Textinput

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import Header from "./components/Header";
 
 export default function App() {
   const appName = "First Mobile App";
+  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header appName={appName} />
+      <TextInput
+        value={text}
+        onChangeText={(inputText) => {
+          setText(inputText);
+        }}
+      />
+      <Text>{text}</Text>
     </View>
   );
 }
