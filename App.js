ed67b3a9b56a4938f37dd003497a6592f0ec14c7
Han Nie <nie.han@northeastern.edu>
2024-09-12 12:06:35 -0700

    fixed order

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
   const appName = "First Mobile App";
   return (
     <View style={styles.container}>
-      <Text>Welcome to {appName}</Text>
       <StatusBar style="auto" />
+      <Header appName={appName} />
     </View>
   );
 }
