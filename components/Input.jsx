7001635f506773820dc241337899fecfea7829cf
Han Nie <nie.han@northeastern.edu>
2024-09-19 11:18:54 -0700

    added confirm button

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,31 +1,41 @@
-import {Text, TextInput, View} from "react-native";
-import {useState} from "react"
+import { Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { Button } from "react-native";
 
-export default function Input({shouldFocus}) {
+export default function Input({ shouldFocus }) {
   const [text, setText] = useState("");
-  const [isFocused, setIsFocused] = useState(false)
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleConfirm = () => {
+    console.log(text);
+  };
 
   return (
-      <View>
-        <TextInput
-            style={{height: 40}}
-            placeholder="Input goes here"
-            value={text}
-            onChangeText={(inputText) => {
-              setText(inputText);
-            }}
-            autoFocus={shouldFocus}
-            onFocus={() => setIsFocused(true)}
-            onBlur={() => {
-              setIsFocused(false)
-            }}
-        />
-        {isFocused &&
-            text.length > 0 &&
-            <Text>Character count: {text.length}</Text>}
-        {!isFocused && (
-            <Text>{text.length >= 3 ? "Thank you"
-                : "Please type more than 3 characters"}</Text>)}
-      </View>
+    <View>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder="Input goes here"
+        value={text}
+        onChangeText={(inputText) => {
+          setText(inputText);
+        }}
+        autoFocus={shouldFocus}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => {
+          setIsFocused(false);
+        }}
+      />
+      {isFocused && text.length > 0 && (
+        <Text>Character count: {text.length}</Text>
+      )}
+      {!isFocused && (
+        <Text>
+          {text.length >= 3
+            ? "Thank you"
+            : "Please type more than 3 characters"}
+        </Text>
+      )}
+      <Button title="Confirm" onPress={handleConfirm} />
+    </View>
   );
 }
