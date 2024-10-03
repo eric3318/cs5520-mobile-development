9126cd63773ad948f9bbe10af6913fe71c6405cc
Han Nie <nie.han@northeastern.edu>
2024-10-03 11:30:25 -0700

    in class activity - createNativeStackNavigator

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import Home from "./components/Home";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
