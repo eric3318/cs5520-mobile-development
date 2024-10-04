12b709f64a6d2fb15ee8dfbb001bea5b21ed7a31
Han Nie <nie.han@northeastern.edu>
2024-10-03 22:22:52 -0700

    using hook to navigate to details screen and removed onClick prop that accepts a callback

--- components/GoalItem.jsx
+++ components/GoalItem.jsx
@@ -1,11 +1,18 @@
 import { Button, StyleSheet, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-export default function GoalItem({ item, onDelete, onClick }) {
+export default function GoalItem({ item, onDelete }) {
+  const navigation = useNavigation();
+
+  const navigateToDetails = () => {
+    navigation.navigate("Details", { goal: item });
+  };
+
   return (
     <View style={styles.textContainer}>
       <Text style={styles.text}>{item.text}</Text>
       <Button title="X" onPress={() => onDelete(item.id)} />
-      <Button title="i" onPress={() => onClick(item)} />
+      <Button title="i" onPress={navigateToDetails} />
     </View>
   );
 }
