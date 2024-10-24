b4bdcdb89f2f89488329590268c0ca8ec4998450
Han Nie <nie.han@northeastern.edu>
2024-10-23 22:16:57 -0700

    in class activity - delete data from firestore

--- firebase/firestoreHelper.js
+++ firebase/firestoreHelper.js
@@ -1,10 +1,19 @@
 import { database } from "./firebaseSetup";
 
 import { collection, addDoc } from "firebase/firestore";
+import { doc, deleteDoc } from "firebase/firestore";
 
 export async function writeToDB(data, collectionName) {
   try {
     await addDoc(collection(database, collectionName), data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function deleteFromDB(id, collectionName) {
+  try {
+    await deleteDoc(collection(database, collectionName, id));
   } catch (err) {
     console.log(err);
   }
